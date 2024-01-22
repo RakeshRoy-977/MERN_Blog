5489a0d17fd22d745cac4931431016c3f672c5d6
RakeshRoy-977 <15rakeshroy@gmail.com>
2024-01-22 14:21:52 +0530

    react js

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import Header from "./Components/Header";
+import Auth from "./Components/Auth";
+import Blogs from "./Components/Blogs";
+import UserBlogs from "./Components/UserBlogs";
+import BlogDetails from "./Components/BlogDetails";
+import AddBlog from "./Components/AddBlog";
+
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/myBlogs" element={<UserBlogs />} />
+        <Route path="/myBlogs/:id" element={<BlogDetails />} />
+        <Route path="/blogs/add" element={<AddBlog />} />
       </Routes>
     </>
   );
